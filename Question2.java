059760d514053715009453d69f15a194161c3406
45tera <103948042+45tera@users.noreply.github.com>
2022-04-19 13:33:04 +0800

    Q2_Attempt2
    Fixed the bugs after testing on Replit

--- Question2.java
+++ Question2.java
@@ -4,7 +4,7 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    /**
+      /**
      * Prompts user for height and weight, calculate and print out the BMI of the user
      * 
      * Hint: in.nextDouble() to read double
@@ -13,20 +13,20 @@ public class Question2
     Scanner in = new Scanner(System.in);
     
     //Prompt user for Height 
-    System.out.println("Enter your Height (in kg): ");
+    System.out.println("Enter your Height (in m): ");
     
     //Height input
     double height = in.nextDouble();
     
     //Prompt user for Weight
-    System.out.println("Enter your Weight (in m): ");
+    System.out.println("Enter your Weight (in kg): ");
     
     //Weight input 
     double weight = in.nextDouble();
     
     //Calculate and print out BMI
-    double BMI = height / weight ; //calculate BMI
-    System.out.println("Your BMI:" + BMI);
+    double BMI = weight /(height * height); //calculate BMI
+    System.out.println("Your BMI:" + BMI); //print out BMI
     
   }
 }
