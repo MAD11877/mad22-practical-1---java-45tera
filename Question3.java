8e2181b619c84b8f3675c0d8a727958e7d40300e
45tera <103948042+45tera@users.noreply.github.com>
2022-04-19 13:47:52 +0800

    Q3_Attempt1
    Did Q3 and checked again in Replit to ensure it is working.

--- Question3.java
+++ Question3.java
@@ -9,8 +9,20 @@ public class Question3
      * 
      * Hint: in.nextInt() to read integer
      */
-     
+    //Creates Scanner
     Scanner in = new Scanner(System.in);
+    
+    //Prompt user for integer
+    System.out.println("Enter a number: ");
+    
+    //Read,Store the input of the user
+    int number = in.nextInt();
+    
+    //Multiply the number - as the number will be overriden (not really a good method)
+    number = number * number ;
+      
+    //Print the number 
+    System.out.println("The squared product of the number entered is " + number);
     
   }
 }
