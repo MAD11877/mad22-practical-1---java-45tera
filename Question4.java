1674b97b37e596057c752da0d837cb18071513e3
45tera <103948042+45tera@users.noreply.github.com>
2022-04-19 23:17:08 +0800

    Q4_Attempt1
    Tried question 4 and checked it against Replit

--- Question4.java
+++ Question4.java
@@ -16,8 +16,23 @@ public class Question4
      * 
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
-     
+    
+    //Create Scanner
     Scanner in = new Scanner(System.in);
     
+    //Prompt user for integer
+    System.out.println("Enter a number:");
+    
+    //Read and Save the variable
+    int rows = in.nextInt();
+    
+    //print the rows
+    for (int x = rows -1;x >=0; x--){
+      String line = "*";
+      for (int y=0;y<x;y++){
+        line += " *";
+      } 
+      System.out.println(line);
+    }
   }
 }
