9f5f9653ab06511bbf817033fca94b56a126da56
45tera <103948042+45tera@users.noreply.github.com>
2022-04-20 00:06:25 +0800

    Q5_Attempt1
    Tried to do question 1, only last part left on the access/find the mode to be completed.

--- Question5.java
+++ Question5.java
@@ -26,7 +26,39 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-    Scanner in = new Scanner(System.in);
+     //Create a Scanner
+     Scanner in = new Scanner(System.in);
+    
+     //Prompt the user for input 
+     System.out.println("Enter a number:");
+
+     //Read,Save the input
+     int number = in.nextInt();
+    
+     //Create an Array List
+     ArrayList<Integer> numberlist = new ArrayList<>();
+
+     //for loop to loop through the numbers
+     for(int x=0; x<number; x++ ){
+        //Prompt the user for input 
+        System.out.println("Enter a number:");
+      
+        //Read,Save the input
+        int number2 = in.nextInt();
+
+        //Add number into array
+        numberlist.add(number2);
+     }
+    
+    
+   
+     //Access and find the mode
+
+
+     //Print the mode
+     System.out.println("The mode is: " + mode);
+
+
     
   }
 }
